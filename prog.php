4042fbeb304aff9eb62ca28a07936283059fc270
ishan <ishan.hcl.tab@gmail.com>
2020-09-13 00:09:30 +0530

    complete dummy Music player website

---
+++ prog.php
@@ -0,0 +1,20 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title>Q4</title>
+</head>
+<body>
+	<?php
+	$num=5;
+	echo'number is $num<br>';
+	echo"number is $num<br>";
+	print($num++);
+	print(<br>);
+	$fname="Sangeeta";
+	$lname="Arya"
+	$lname=$fname."Sharma";
+	print($lname)
+	?>
+
+</body>
+</html>
